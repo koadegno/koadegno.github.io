13fd3c349ab07be624ce3632c1e2a3f263decc4f
koadegno <vinove9@hotmail.fr>
2024-11-04 18:24:55 +0100

    update script for reading json project

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,9 @@
 // Function to load project cards dynamically from JSON file
 function loadProjects() {
-    fetch('projects/projects.json')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            return response.json();
-        })
+    fetch('../projects/projects.json')
+        .then(response => response.json())
         .then(data => {
             const projectsSection = document.getElementById('projects-section');
-
             data.forEach(project => {
                 const card = document.createElement('div');
                 card.className = 'project-card';
