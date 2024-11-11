48bf0917d21b9e3f3d3a5d4eac940a82a5676c74
koadegno <vinove9@hotmail.fr>
2024-11-11 22:55:14 +0100

    Project page done

--- assets/script.js
+++ assets/script.js
@@ -9,67 +9,74 @@ function loadProjects() {
 				card.classList.add("rounded-xl", "border", "bg-card", "text-card-foreground", "shadow", "flex", "flex-col");
 				// card.className = 'project-card';
 
-				const card_img = document.createElement("div");
-				card_img.classList.add("flex", "flex-col", "space-y-1.5", "p-6");
-				card_img.innerHTML = `<img class="h-32 max-w-full object-cover object-top" src="${project.image}" width="500" height="300" alt="${project.title} style="color:transparent"">`;
+				// Image loading
+				const cardImg = document.createElement("div");
+				cardImg.classList.add("flex", "flex-col", "space-y-1.5", "p-6");
+				cardImg.innerHTML = `<img class="h-32 max-w-full object-cover object-top" src="${project.image}" width="500" height="300" alt="${project.title} style="color:transparent"">`;
+				card.appendChild(cardImg);
 
-				card.appendChild(card_img);
-
-				const card_text = document.createElement("div");
-				card_text.classList.add("p-6", "pt-0", "flex", "flex-col", "gap-2")
-
-				card_text.innerHTML = `<h3 class="font-semibold leading-none tracking-tight">
+				// Title and description text loading
+				const cardText = document.createElement("div");
+				cardText.classList.add("p-6", "pt-0", "flex", "flex-col", "gap-2")
+				cardText.innerHTML = `<h3 class="font-semibold leading-none tracking-tight">
 				${project.title}
 				</h3>
-				<div class"max-w-full text-pretty font-sans text-xs text-muted-foreground dark:prose-invert flex">
+				<div class"prose max-w-full text-pretty font-sans text-xs text-muted-foreground dark:prose-invert">
 					<p>${project.description}</p>
 				</div>
-				
-				<div class="mt-2 flex-col p-6 pt-0 flex ">
-					<a href="${project.link}">View Project</a>
-				
-				</div>
 				`;
-// make the View Project  link be at the same position in each cards
-				card.appendChild(card_text);
+				card.appendChild(cardText);
 
-				const card_tools = document.createElement('div');
-				card_tools.classList.add("p-6","pt-0", "flex", "h-full", "flex-col", "items-start", "justify-between", "gap-4");
-				const card_container_tools = document.createElement("div");
+				// <div class="mt-2 flex-col p-6 pt-0 flex ">
+				// 	<a href="${project.link}">View Project</a>
+				
+				// </div>
+				// make the View Project  link be at the same position in each cards
 
-				card_container_tools.classList.add("mt-2", "flex", "flex-wrap", "gap-1", )
+				// Add tools to the card
+				const cardTools = document.createElement('div');
+				cardTools.classList.add("p-6","pt-0", "flex",  "flex-col", "items-start", "justify-between", "gap-4",);
+
+				const toolsContainer = document.createElement("div");
+				toolsContainer.classList.add("mt-2", "flex", "flex-wrap", "gap-1" )
 
 				project.tools.forEach(tool => {
-					tools_html = document.createElement('div');
-					tools_html.classList.add("inline-flex", "items-center", "rounded-md", "border", "font-semibold", "transition-colors", "focus:outline-none", "focus:ring-2", "focus:ring-ring", "focus:ring-offset-2", "border-transparent", "bg-secondary", "text-secondary-foreground", "hover:bg-secondary/80", "px-1", "py-0", "text-[10px]",);
-					tools_html.innerText = tool;
-					card_tools.appendChild(tools_html);
+					toolsContainer.innerHTML += `
+					<div class="inline-flex items-center rounded-md border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80 px-1 py-0 text-[10px]">
+					${tool}
+					</div>\n`
 
 				});
-				card_container_tools.appendChild(card_tools);
-				
-				card.appendChild(card_container_tools);
 
-                // card.innerHTML = `
-                //     <a href="${project.link}">
-                //         <img src="${project.image}" alt="${project.title}">
-                //         <h3>${project.title}</h3>
-				// 	</a>
-				// 	<div class="year">${project.year || 'N/A'}</div>
-				// 	<p alt="No Description"> ${project.description}</p>
-                // `;
+				cardTools.appendChild(toolsContainer);
 
-				// card.innerHTML = `
-                //     <img src="${project.image}" alt="${project.title}">
-                //     <div class="content">
-                //         <a href="${project.link}">
-                //             <h3>${project.title}</h3>
-                //         </a>
-                //         <div class="year">${project.year || 'N/A'}</div>
-                //         <p>${project.description || 'No Description'}</p>
-                //     </div>
-                // `;
+				// Buttons
+				if ((project.code_link && project.code_link != "") || (project.link && project.link != "")){
 
+					const buttonsDiv = document.createElement("div");
+					buttonsDiv.classList.add("flex", "flex-row", "flex-wrap", "items-start", "gap-1")
+					// Source button
+					if (project.code_link && project.code_link != "") {
+						buttonsDiv.innerHTML += `<a target="_blank" href="${project.code_link}"> <div class="items-center rounded-md border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80 flex gap-2 px-2 py-1 text-[10px]"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github size-3"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path><path d="M9 18c-4.51 2-5-2-7-2"></path></svg>Source</div></a></div>`
+	
+					}
+	
+					// Description page link
+					if (project.link && project.link != "") {
+						buttonsDiv.innerHTML += `<a target="_blank" href="${project.link}"> <div class="items-center rounded-md border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80 flex gap-2 px-2 py-1 text-[10px]"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-external-link size-3">
+      <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+      <polyline points="15 3 21 3 21 9"></polyline>
+      <line x1="10" y1="14" x2="21" y2="3"></line>
+    </svg>Description Page</div></a></div>`
+					}
+					cardTools.appendChild(buttonsDiv)
+
+				}
+
+
+
+
+				card.appendChild(cardTools);
 
                 projectsSection.appendChild(card);
             });
